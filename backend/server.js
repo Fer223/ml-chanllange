1a58861a35cb45b176667d093674bab775f9d494
LUNA BARATTI Fernando <LunaF@sancristobal.com.ar>
2021-04-11 20:59:23 -0300

    Added axios and cors - created get for search and item description

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
 const PORT = 3000;
+
+app.get('/items', cors(), (req, res) => {
+    axios({
+        method: 'get',
+        url: `https://api.mercadolibre.com/sites/MLA/search?q=${req.query.search}`,
+        responseType: 'stream'
+    }).then(response => {
+        response.data.pipe(res);
+    })
+})
+
+app.get('/items/:id/description', cors(), (req, res) => {
+    axios({
+        method: 'get',
+        url: `https://api.mercadolibre.com/items/${req['params'].id}/description`,
+        responseType: 'stream'
+    }).then(response => {
+        response.data.pipe(res);
+    })
+})
 
 app.listen(PORT, () => {
     console.log('Express is listening on port ', PORT)
